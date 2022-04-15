f40c5f055e6cc27c4793b2451f7d67a224c1f3b2
yeoung <yeoung.dev@gmail.com>
2022-04-15 23:19:32 +0900

    react book search ap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import {
   Routes,
 } from "react-router-dom"
 
-import Join from "./Routes/Home"
+import Join from "./Routes/Join"
 import Search from "./Routes/Search"
 import Login from "./Routes/LogIn"
 import Home from "./Routes/Home"
 
 function App() {
   return(
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/Home" element={<Home />} exact />
+        <Route path="/Home" element={<Home />} />
         <Route path="/Search" element={<Search />} />
         <Route path="/Join" element={<Join />} />
         <Route path="/Login" element={<Login />} />
