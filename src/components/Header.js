6c922a70f639e7a2eab168a6b28ebd1bf8890880
yeoung <yeoung.dev@gmail.com>
2022-07-16 00:24:34 +0900

    7.16 2차 수정완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom";
-import {HeaderDiv, } from "../style/HeaderCSS"
+import { Link } from 'react-router-dom';
+import { HeaderDiv } from '../style/HeaderCSS'
 
 const Header = () => {
   return (
-    <>
-      <HeaderDiv>
-        <h1 className="header_title">우리는 더 많은 책을 읽어야해</h1>
-        <Link to ="/" className="header_cate">메인</Link>
-        <Link to ="/Search" className="header_cate">검색</Link>
-        <Link to ="/Memo" className="header_cate">문장</Link>
-      </HeaderDiv>
-    </>
+    <HeaderDiv>
+      <div className='header'>
+      <div className='content'>
+        <Link to ='/' className='content-index'>INDEX</Link>
+        <Link to ='/Search' className='content-search'>SEARCH</Link>
+        <Link to ='/Memo' className='content-memo'>MEMO</Link>
+      </div>
+      </div>
+    </HeaderDiv>
+  
   )
 }
 
--- src/style/HeaderCSS.js
+++ src/style/HeaderCSS.js
@@ -1,19 +1,47 @@
-import styled from "@emotion/styled"
+import styled from '@emotion/styled'
+
 
 const HeaderDiv = styled.div `
-  width: 96%;
-  margin-top: 50px;
-  margin-left: 25px;
-  padding-bottom: 20px;
-  border-bottom: 3px solid #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
 
-  .header_title {
-    font-size: 30px;
+  .title {
+    color: #aa8eed;
+    font-size: 90px;
+    display: block;
+    text-align: center;
+    font-family: 'Fugaz One', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
   }
 
-  .header_cate {
-    margin-right:30px;
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .content-index,
+  .content-search,
+  .content-memo {
+    font-size: 60px;
+    font-family: 'Fugaz One';
+    text-shadow: 2px 2px 0px #eee, 4px 4px 0px #000
+    }
+
+  .content-index {
+    color: #f1de95;
+  }
+
+  .content-search {
+    color: #f19995;
+  }
+
+  .content-memo {
+    color:#bcdce4;
   }
 `
+
 
 export { HeaderDiv }
